0e8ba684c4f02545d2db23a08902f68442854260
Ashraful Alam <alamashraful1003@gmail.com>
2023-06-08 08:00:22 +0600

    lamp light added in Detail Item Screen

--- lib/views/detail_item/detail_item_sceen.dart
+++ lib/views/detail_item/detail_item_sceen.dart
@@ -11,7 +11,6 @@ import '../smart_mode/mode_card.dart';
 class DetailItemScreen extends StatelessWidget {
   const DetailItemScreen({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,17 +18,13 @@ class DetailItemScreen extends StatelessWidget {
         child: Column(
           children: [
             Container(
-              width: MediaQuery
-                  .of(context)
-                  .size
-                  .width,
+              width: MediaQuery.of(context).size.width,
               color: hexToColor(roombg),
               child: Container(
                 decoration: BoxDecoration(
-                  color: hexToColor(mains2),
-                  borderRadius:
-                  BorderRadius.only(bottomLeft: Radius.circular(35)),
-                ),
+                    color: hexToColor(mains2),
+                    borderRadius:
+                        BorderRadius.only(bottomLeft: Radius.circular(35))),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -38,94 +33,102 @@ class DetailItemScreen extends StatelessWidget {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             SafeArea(
-                              child: Row(
-                                children: [
-                                  Row(
-                                    children: [
-                                      Container(
-                                        margin: EdgeInsets.only(left: 18),
-                                        child: GestureDetector(
-                                            onTap: () {},
-                                            child: const Icon(
-                                              Icons.arrow_back_ios,
-                                              color: Colors.white,
-                                            )),
-                                      ),
-                                      TextWidget(
-                                          text: 'Back',
-                                          size: 12,
-                                          weight: FontWeight.w400,
-                                          color: Colors.white),
-                                    ],
-                                  ),
-                                  Container(
-                                    width:
-                                    MediaQuery
-                                        .of(context)
-                                        .size
-                                        .width - 150,
-                                    alignment: Alignment.center,
-                                    child: TextWidget(
-                                        text: 'Lamp',
-                                        size: 24,
-                                        weight: FontWeight.w600,
-                                        color: Colors.white),
-                                  ),
-                                ],
-                              ),
-                            ),
+                                child: Row(children: [
+                              Row(children: [
+                                Container(
+                                    margin: EdgeInsets.only(left: 18),
+                                    child: GestureDetector(
+                                        onTap: () {},
+                                        child: const Icon(
+                                          Icons.arrow_back_ios,
+                                          color: Colors.white,
+                                        ))),
+                                TextWidget(
+                                    text: 'Back',
+                                    size: 12,
+                                    weight: FontWeight.w400,
+                                    color: Colors.white),
+                              ]),
+                              Container(
+                                  width:
+                                      MediaQuery.of(context).size.width - 150,
+                                  alignment: Alignment.center,
+                                  child: TextWidget(
+                                      text: 'Lamp',
+                                      size: 24,
+                                      weight: FontWeight.w600,
+                                      color: Colors.white)),
+                            ])),
                             Container(
                                 padding: EdgeInsets.only(left: 17, top: 10),
                                 child: Column(
                                     crossAxisAlignment:
-                                    CrossAxisAlignment.start,
+                                        CrossAxisAlignment.start,
                                     children: [
                                       Column(
-                                        crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                        children: [
-                                          TextWidget(
-                                              text: 'Dining Room',
-                                              size: 14,
-                                              weight: FontWeight.w400,
-                                              color: Colors.white),
-                                          Container(
-                                            height: 24,
-                                            child: Image.asset(
-                                              'assets/icon_off.png',
-                                              fit: BoxFit.fill,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            TextWidget(
+                                                text: 'Dining Room',
+                                                size: 14,
+                                                weight: FontWeight.w400,
+                                                color: Colors.white),
+                                            SizedBox(height: 12,),
+                                            Container(
+                                                height: 24,
+                                                child: Image.asset(
+                                                    'assets/icon_off.png',
+                                                    fit: BoxFit.fill)),
+                                          ]),
+                                      SizedBox(height: 26,),
                                       Column(
                                           crossAxisAlignment:
-                                          CrossAxisAlignment.start,
+                                              CrossAxisAlignment.start,
                                           children: [
                                             Row(
-                                              crossAxisAlignment:
-                                              CrossAxisAlignment.center,
-                                              children: [
-                                                TextWidget(
-                                                    text: '80',
-                                                    size: 50,
-                                                    weight: FontWeight.w600,
-                                                    color: Colors.white),
-                                                TextWidget(
-                                                    text: '%',
-                                                    size: 24,
-                                                    weight: FontWeight.w600,
-                                                    color: Colors.white),
-                                              ],
-                                            ),
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.center,
+                                                children: [
+
+                                                  TextWidget(
+                                                      text: '80',
+                                                      size: 50,
+                                                      weight: FontWeight.w600,
+                                                      color: Colors.white),
+                                                  TextWidget(
+                                                      text: '%',
+                                                      size: 24,
+                                                      weight: FontWeight.w600,
+                                                      color: Colors.white),
+                                                ]),
                                             TextWidget(
                                                 text: 'Brightness',
                                                 size: 16,
                                                 weight: FontWeight.w400,
-                                                color: Colors.white)
+                                                color: Colors.white),
                                           ]),
                                     ])),
                           ]),
+                      Positioned(
+                          right: 95,
+                          top: 100,
+                          child: Container(
+                            height: 80,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              // color: Colors.red,
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.orange.withOpacity(0.6),
+                                      spreadRadius: 30,
+                                      blurRadius: 60,
+                                      offset: const Offset(
+                                          25, 60)
+                                  ),
+                                ],
+                                borderRadius: BorderRadius.circular(90)),
+                          )),
                       Positioned(
                           right: 40,
                           child: Container(
@@ -136,92 +139,91 @@ class DetailItemScreen extends StatelessWidget {
                               ))),
                     ]),
                     Container(
-                        margin: EdgeInsets.only(
-                            left: 18, right: 18, bottom: 18),
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                      margin: EdgeInsets.only(left: 18, right: 18, bottom: 18, top: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextWidget(
+                              text: 'Insensity',
+                              size: 14,
+                              weight: FontWeight.w600,
+                              color: Colors.white),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Row(
                             children: [
-                            TextWidget(
-                            text: 'Insensity',
-                            size: 14,
-                            weight: FontWeight.w600,
-                            color: Colors.white),
-                        SizedBox(
-                          height: 12,
-                        ),
-                        Row(
-                            children: [
-                            ImageWidget(
-                            image: 'assets/light_off.png',
-                            width: 34,
-                            height: 34),
-                        SliderCard(),
-                        ImageWidget(
-                        image: 'assets/light_on.png',
-                        width: 42,
-                        height: 42),
+                              ImageWidget(
+                                  image: 'assets/light_off.png',
+                                  width: 34,
+                                  height: 34),
+                              SliderCard(),
+                              ImageWidget(
+                                  image: 'assets/light_on.png',
+                                  width: 42,
+                                  height: 42),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Divider(
+                            thickness: 2,
+                            color: hexToColor(roombg),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextWidget(
+                              text: 'Usages',
+                              size: 14,
+                              weight: FontWeight.w600,
+                              color: Colors.white),
+                          Usages(title: 'Use Today', unit: '50 watt'),
+                          Usages(title: 'Use Week', unit: '500 kwh'),
+                          Usages(title: 'Use Month', unit: '5000 kwh'),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
-                SizedBox(
-                  height: 20,
-                ),
-                Divider(
-                  thickness: 2,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: hexToColor(mains2),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
                   color: hexToColor(roombg),
+                  borderRadius:
+                      BorderRadius.only(topRight: Radius.circular(35)),
                 ),
-                SizedBox(
-                  height: 20,
+                child: Column(
+                  children: [
+                    Container(
+                        margin:
+                            const EdgeInsets.only(left: 16, right: 16, top: 16),
+                        child: SeeAll(
+                            text1: 'Schedule', text2: 'icon', text3: '4')),
+                    Container(
+                      margin: const EdgeInsets.only(
+                          left: 18, right: 18, bottom: 18),
+                      child: ListView.builder(
+                          physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: 4,
+                          itemBuilder: (context, index) {
+                            return ModeCard();
+                          }),
+                    ),
+                  ],
                 ),
-                TextWidget(
-                    text: 'Usages',
-                    size: 14,
-                    weight: FontWeight.w600,
-                    color: Colors.white),
-                Usages(title: 'Use Today', unit: '50 watt'),
-                Usages(title: 'Use Week', unit: '500 kwh'),
-                Usages(title: 'Use Month', unit: '5000 kwh'),
-                ],
               ),
             ),
           ],
         ),
       ),
-    ),
-    Container(
-    decoration: BoxDecoration(
-    color: hexToColor(mains2),
-    ),
-    child: Container(
-    decoration: BoxDecoration(
-    color: hexToColor(roombg),
-    borderRadius:
-    BorderRadius.only(topRight: Radius.circular(35)),
-    ),
-    child: Column(
-    children: [
-    Container(
-    margin:
-    const EdgeInsets.only(left: 16, right: 16, top: 16),
-    child: SeeAll(
-    text1: 'Schedule', text2: 'icon', text3: '4')),
-    Container(
-    margin: const EdgeInsets.only(
-    left: 18, right: 18, bottom: 18),
-    child: ListView.builder(
-    physics: const NeverScrollableScrollPhysics(),
-    shrinkWrap: true,
-    itemCount: 4,
-    itemBuilder: (context, index) {
-    return ModeCard();
-    }),
-    ),
-    ],
-    ),
-    ),
-    ),
-    ],
-    ),
-    ),
     );
   }
 }
